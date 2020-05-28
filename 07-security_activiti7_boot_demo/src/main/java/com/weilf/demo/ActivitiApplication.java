7518e2e6b35b4d9a39a2f6f1602b4ee265f30326
weil-f <741847605@qq.com>
2020-05-28 17:57:26 +0800

    初始化activiti7

---
+++ 07-security_activiti7_boot_demo/src/main/java/com/weilf/demo/ActivitiApplication.java
@@ -0,0 +1,20 @@
+package com.weilf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: weilai
+ * @CreateDate: 2020/5/27 14:49
+ * @UpdateUser: weilai
+ * @UpdateDate: 2020/5/27 14:49
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class,args);
+    }
+}
