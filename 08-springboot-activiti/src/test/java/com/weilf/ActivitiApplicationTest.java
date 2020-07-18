85d92ba3bba2cfe3f89b2a4811b06918c6dd5020
weil-f <741847605@qq.com>
2020-07-18 11:32:06 +0800

    修改

--- 08-springboot-activiti/src/test/java/com/weilf/ActivitiApplicationTest.java
+++ 08-springboot-activiti/src/test/java/com/weilf/ActivitiApplicationTest.java
@@ -1,7 +1,9 @@
 package com.weilf;
 
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
@@ -9,6 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Package: com.weilf
@@ -27,11 +32,47 @@ public class ActivitiApplicationTest {
 	@Autowired
 	private TaskService taskService;
 
+	@Autowired
+	private RepositoryService repositoryService;
+
+	/**
+	 * 根据key 部署任务
+	 */
 	@Test
 	public void startTest(){
-		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("hello");
+	//	ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("test001");
+		Deployment test = repositoryService.createDeployment().name("复杂请假流程").addClasspathResource("bpmn/" + "test002.bpmn").deploy();
+		System.out.println(test.getId());
+		System.out.println(test.getKey());
+		System.out.println(test.getName());
+	}
+
+	/**
+	 * 根据流程定义的key 启动任务
+	 */
+	@Test
+	public void startProcess(){
+		/*ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("test00002");*/
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("test00002", "1");
+		System.out.println(processInstance.getId());
 		System.out.println(processInstance.getName());
 	}
+
+
+	/**
+	 * 查询并完成任务
+	 */
+	@Test
+	public void tastQuery(){
+		Task task = taskService.createTaskQuery().processDefinitionId("test00002:1:4").singleResult();
+	/*	HashMap<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("day",2);
+		taskService.complete(task.getId(),dataMap);*/
+		taskService.complete(task.getId());
+		 task = taskService.createTaskQuery().processDefinitionId("test00002:1:4").singleResult();
+		System.out.println(task.getAssignee());
+	}
+
 
 	@Test
 	public void overTask(){
